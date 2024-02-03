3ac7c4bb47ebeb6da5e6d23dce04ced646caf018
Alex Ansari <alex.r.ansari@gmail.com>
2024-02-03 02:09:38 -0800

    Added fzf and webdevicons

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,5 +3,22 @@
 --
 -- See the kickstart.nvim README for more information
 return {
+	{
+		"junegunn/fzf",
+		build = "./install --all"
+	},
+
+	"junegunn/fzf.vim",
+
+	{
+		"nvim-tree/nvim-web-devicons",
+		lazy = true,
+		opts = {
+			-- globally enable default icons (default to false)
+			-- will get overriden by `get_icons` option
+			default = true,
+		},
+	},
+
 	"vimwiki/vimwiki",
 }
