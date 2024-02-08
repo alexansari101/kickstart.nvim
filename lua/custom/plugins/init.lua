98f394ca26bf3f7699a95c7321f0e2b9771cd451
Alex Ansari <alex.r.ansari@gmail.com>
2024-02-07 23:19:03 -0800

    Added nvim-surround.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -38,6 +38,15 @@ return {
 	},
 
 	{
+		"kylechui/nvim-surround",
+		version = "*", -- Use for stability; omit to use `main` branch for the latest features
+		event = "VeryLazy",
+		config = function()
+			require("nvim-surround").setup({})
+		end
+	},
+
+	{
 		"iamcco/markdown-preview.nvim",
 		cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
 		ft = { "markdown" },
