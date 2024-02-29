b602b6d604a78a267fd3842c312134f8660636d4
Alex Ansari <alex.r.ansari@gmail.com>
2024-02-28 22:35:51 -0800

    Added Trouble

--- init.lua
+++ init.lua
@@ -301,9 +301,11 @@ require('lazy').setup {
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>h'] = { name = '[H]unks', _ = 'which_key_ignore' },
         ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        ['<leader>x'] = { name = '[T]rouble', _ = 'which_key_ignore' },
       }
     end,
   },
